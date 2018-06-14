8fc2314af76b0c8c07f7eed3e297f4892e62a1b5
Christoph Held <c-held@web.de>
2018-06-14 14:20:52 +0200

    added insecure deployment

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -4,4 +4,4 @@ IMAGE=$1
 oc project demo-broker || oc new-project demo-broker
 CA=`oc get secret -n kube-service-catalog -o go-template='{{ range .items }}{{ if eq .type "kubernetes.io/service-account-token" }}{{ index .data "service-ca.crt" }}{{end}}{{"\n"}}{{end}}' | tail -n 1`
 
-oc process -f openshift/demo-broker.yaml -p IMAGE=$IMAGE -p BROKER_CA_CERT=$CA | oc apply -f -
+oc process -f openshift/demo-broker-insecure.yaml -p IMAGE=$IMAGE -p BROKER_CA_CERT=$CA | oc apply -f -
